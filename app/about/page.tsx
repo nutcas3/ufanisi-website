82d6b33194e8b403f7b468e50f539ee885983d2b
mauricenyanja <mauricee423@gmail.com>
2023-04-25 14:34:58 +0300

    navbar and hero

---
+++ app/about/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Link from 'next/link'
+
+function About() {
+  return (
+    <Link href={`/about/`}>
+      <div className="max-w-3xl mx-auto px-4 py-8">
+        <p className="mt-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+          interdum libero vitae leo iaculis, eu laoreet quam egestas.
+          Pellentesque habitant morbi tristique senectus et netus et malesuada
+          fames ac turpis egestas.
+        </p>
+
+        <p className="mt-4">
+          Nulla hendrerit lectus at magna sollicitudin, vel scelerisque magna
+          tristique. Sed vitae eros eu ex bibendum consectetur.
+        </p>
+      </div>
+    </Link>
+  );
+}
+export default About;
---
+++ app/whatwedo/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Whatwedo
+() {
+  return (
+    <div>
+        Whatwedo
+
+    </div>
+  )
+}
+
+export default Whatwedo
