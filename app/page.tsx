2434f5405a4da69cdc5d491bb6b36e609530e64a
mauricenyanja <mauricee423@gmail.com>
2023-04-26 16:35:13 +0300

    added owl caroules

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
-import HeroBanner from "./components/HeroBanner";
+"use client"
 
+import HeroBanner from "./components/HeroBanner"
+
+import { useEffect } from 'react'
 export default function Home() {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap');
+  }, []);
   return <>
   <HeroBanner/>
   </>;
