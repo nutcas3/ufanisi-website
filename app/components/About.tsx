feb4012c6124a6b806d7860204406fa4b63b0c95
mauricenyanja <mauricee423@gmail.com>
2023-04-29 03:28:19 +0300

    about component working

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -13,7 +13,11 @@ function About() {
                 className="about-img"
                 data-parallax="scroll"
                 data-image-src="img/about.jpg">
-                <img src="https://cdn.pixabay.com/photo/2023/04/13/23/07/sea-turtle-7923608__340.png" alt="this is us" fluid />
+                <img
+                  src="https://cdn.pixabay.com/photo/2023/04/13/23/07/sea-turtle-7923608__340.png"
+                  alt="this is us"
+                  fluid
+                />
               </div>
             </div>
             <div className="col-lg-6">
@@ -22,29 +26,12 @@ function About() {
                 <h2>Worldwide non-profit charity organization</h2>
               </div>
               <div className="about-tab">
-                <Tabs className="nav-pills nav-justified">
-                  <Tab title="linkNav" className="nav-item">
-                    <NavLink
-                      className="nav-link active"
-                      eventKey="tab-content-1">
-                      About
-                    </NavLink>
-                  </Tab>
-                  <Tab title="linkNav" className="nav-item">
-                    <NavLink className="nav-link" eventKey="tab-content-2">
-                      Mission
-                    </NavLink>
-                  </Tab>
-                  <Tab title="linkNav" className="nav-item">
-                    <NavLink className="nav-link" eventKey="tab-content-3">
-                      Vision
-                    </NavLink>
-                  </Tab>
-                </Tabs>
-                <div className="tab-content">
-                  <Tab.Pane
-                    eventKey="tab-content-1"
-                    className="container tab-pane active">
+                <Tabs
+                  defaultActiveKey="profile"
+                  id="justify-tab-example"
+                  className="mb-3"
+                  justify>
+                  <Tab eventKey="home" title="Home">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
                     vitae pellentesque turpis. Donec in hendrerit dui, vel
                     blandit massa. Ut vestibulum suscipit cursus. Cras quis
@@ -53,32 +40,29 @@ function About() {
                     dignissim. Morbi sit amet leo at purus accumsan
                     pellentesque. Vivamus fermentum nisi vel dapibus blandit.
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  </Tab.Pane>
-                  <Tab.Pane
-                    eventKey="tab-content-2"
-                    className="container tab-pane fade">
-                    Sed tincidunt, magna ut vehicula volutpat, turpis diam
-                    condimentum justo, posuere congue turpis massa in mi. Proin
-                    ornare at massa at fermentum. Nunc aliquet sed nisi iaculis
-                    ornare. Nam semper tortor eget est egestas, eu sagittis nunc
-                    sodales. Interdum et malesuada fames ac ante ipsum primis in
-                    faucibus. Praesent bibendum sapien sed purus molestie
-                    malesuada. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit.
-                  </Tab.Pane>
-                  <Tab.Pane
-                    eventKey="tab-content-3"
-                    className="container tab-pane fade">
-                    Aliquam dolor odio, mollis sed feugiat sit amet, feugiat ut
-                    sapien. Nunc eu dignissim lorem. Suspendisse at hendrerit
-                    enim. Interdum et malesuada fames ac ante ipsum primis in
-                    faucibus. Sed condimentum semper turpis vel facilisis. Nunc
-                    vel faucibus orci. Mauris ut mauris rhoncus, efficitur nisi
-                    at, venenatis quam. Praesent egestas pretium enim sit amet
-                    finibus. Curabitur at erat molestie, tincidunt lorem eget,
-                    consequat ligula.
-                  </Tab.Pane>
-                </div>
+                  </Tab>
+                  <Tab eventKey="profile" title="Profile">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    vitae pellentesque turpis. Donec in hendrerit dui, vel
+                    blandit massa. Ut vestibulum suscipit cursus. Cras quis
+                    porta nulla, ut placerat risus. Aliquam nec magna eget velit
+                    luctus dictum. Phasellus et felis sed purus tristique
+                    dignissim. Morbi sit amet leo at purus accumsan
+                    pellentesque. Vivamus fermentum nisi vel dapibus blandit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </Tab>
+                  <Tab eventKey="longer-tab" title="Loooonger Tab">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    vitae pellentesque turpis. Donec in hendrerit dui, vel
+                    blandit massa. Ut vestibulum suscipit cursus. Cras quis
+                    porta nulla, ut placerat risus. Aliquam nec magna eget velit
+                    luctus dictum. Phasellus et felis sed purus tristique
+                    dignissim. Morbi sit amet leo at purus accumsan
+                    pellentesque. Vivamus fermentum nisi vel dapibus blandit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  </Tab>
+
+                </Tabs>
               </div>
             </div>
           </div>
